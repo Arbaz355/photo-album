cb5c9bb46496194231bfbd848fba776920a75ae7
Arbaz355 <arbazakhter355@gmail.com>
2021-09-25 19:03:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core'
+import {PhotoCamera} from '@material-ui/icons'
+import {Button} from '@material-ui/core'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import useStyles from './style'
+
+const cards = [1, 2, 3, 4, 5, 6]
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline/>
+      <AppBar position="relative">
+        <Toolbar>
+            <PhotoCamera className={classes.icon}/>
+            <Typography variant="h6">
+              Photo Album
+            </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.container}>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h3" align="center" color="textPrimary" gutterBottom>
+              Photo Album 
+            </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography> 
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">see my photo</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+          {cards.map ( (card) => (
+            <Grid item key={card} xs={12} sm={6} md={4} >
+              <Card className={classes.card}>
+                <CardMedia className={classes.cardMedia}  component="img" image="https://source.unsplash.com/random" title="Image title" />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h6">Heading</Typography> 
+                  <Typography variant="p" paragraph>
+                    This is a card media just for the demonstration purpose
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">View</Button>
+                  <Button size="small" color="primary">Edit </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+            <Typography variant="h6" gutterBottom>
+              Footer
+            </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary">
+              Something here to give the footer purpose
+            </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary">
+              Copyright © Your Website 2021.
+            </Typography>
+      </footer>
+    </React.Fragment>
   );
 }
 
